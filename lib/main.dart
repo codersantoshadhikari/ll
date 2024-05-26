d47e83631b1d493b3bc4b565bac7cdcf5e133f34
codersantoshadhikari <santoshadhikari.name.np@gmail.com>
2024-05-26 14:51:37 +0545

    added updated

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
+import 'views/home.dart';
+import 'views/settings.dart';
+import 'model/locale.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => LocaleModel(),
+      child: Consumer<LocaleModel>(
+        builder: (context, localeModel, child) => MaterialApp(
+          title: 'Recipes',
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+          ),
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          locale: localeModel.locale,
+          routes: {
+            '/': (context) => const Home(),
+            '/settings': (context) => const Settings(),
+          },
+          initialRoute: '/',
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/settings.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
+
+import '../model/locale.dart';
+
+class Settings extends StatelessWidget {
+  const Settings({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var t = AppLocalizations.of(context)!;
+
+    var selectedLocale = Localizations.localeOf(context).toString();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(t.pageSettingsTitle),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Consumer<LocaleModel>(
+              builder: (context, localeModel, child) => DropdownButton(
+                value: selectedLocale,
+                items: [
+                  DropdownMenuItem(
+                    value: "en",
+                    child: Text(t.pageSettingsInputLanguage("en")),
+                  ),
+                  DropdownMenuItem(
+                    value: "es",
+                    child: Text(t.pageSettingsInputLanguage("es")),
+                  ),
+                  DropdownMenuItem(
+                    value: "ar",
+                    child: Text(t.pageSettingsInputLanguage("ar")),
+                  ),
+                ],
+                onChanged: (String? value) {
+                  if (value != null) {
+                    localeModel.set(Locale(value));
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
